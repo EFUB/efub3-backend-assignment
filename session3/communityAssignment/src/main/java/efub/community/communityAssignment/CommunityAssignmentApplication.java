ad0dd62ae9c04f5acfc6e0b1171eb8cfeda7b3d7
nammsamm <108855492+nammsamm@users.noreply.github.com>
2023-04-07 00:02:48 +0900

    Add files via upload

---
+++ session3/communityAssignment/src/main/java/efub/community/communityAssignment/CommunityAssignmentApplication.java
@@ -0,0 +1,13 @@
+package efub.community.communityAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityAssignmentApplication.class, args);
+	}
+
+}
