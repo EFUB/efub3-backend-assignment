5152124adf37278a774ec5189d30c2c2167fbbee
nammsamm <108855492+nammsamm@users.noreply.github.com>
2023-04-12 00:59:10 +0900

    Add files via upload

---
+++ Community/communityAssignment/src/main/java/efub/community/communityAssignment/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package efub.community.communityAssignment.member.repository;
+
+import efub.community.communityAssignment.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//Spring Data JPA : JpaRepository<엔티티타입,식별자타입>을 상속하여 사용한다.
+public interface MemberRepository extends JpaRepository<Member,Long> {
+    Boolean existsByEmail(String email); // email을 이용하여 찾겠다.
+}
---
+++ Community/communityAssignment/src/test/java/efub/community/communityAssignment/CommunityAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package efub.community.communityAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
