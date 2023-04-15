1bb1345a2320eb0e04428504b1aa405b29e467c3
Chaewon Song <chaewon1019@ewhain.net>
2023-04-15 14:33:32 +0900

    Delete Directory

--- assignment/src/main/java/efub/backend/assignment/member/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package efub.backend.assignment.member.repository;
-
-import efub.backend.assignment.member.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    Boolean existsByEmail(String email);
-}
