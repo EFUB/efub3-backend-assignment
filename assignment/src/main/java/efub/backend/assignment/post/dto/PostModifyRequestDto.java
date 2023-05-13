819cb2623072107dfbfc1e023f303d601775f96c
chhaewxn <chaewon1019@ewhain.net>
2023-05-13 19:52:58 +0900

    🔧 fix : Post, Board 코드 수정

--- assignment/src/main/java/efub/backend/assignment/post/dto/PostModifyRequestDto.java
+++ assignment/src/main/java/efub/backend/assignment/post/dto/PostModifyRequestDto.java
@@ -4,12 +4,15 @@ import lombok.Getter;
 
 /*
 {
+    "writedId" : 1,
     "title" : "수정한 제목",
     "content": "수정한 내용입니다~"
 }
  */
 @Getter
 public class PostModifyRequestDto {
+    private Long writerId;
     private String title;
     private String content;
-}
+
+    }
