185de00f934d4db99a24c141105446e964cb2f2e
choiyounji <yunji71@naver.com>
2023-10-07 22:47:56 +0900

    test: PostRepositoryTest code 작성

--- src/test/java/com/efub/community/domain/board/repository/PostRepositoryTest.java
+++ src/test/java/com/efub/community/domain/board/repository/PostRepositoryTest.java
@@ -2,6 +2,9 @@ package com.efub.community.domain.board.repository;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
@@ -10,41 +13,37 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import com.efub.community.domain.board.domain.Board;
-import com.efub.community.domain.board.domain.Comment;
 import com.efub.community.domain.board.domain.Post;
 import com.efub.community.domain.member.domain.Member;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE, connection = EmbeddedDatabaseConnection.H2)
-public class CommentRepositoryTest {
+public class PostRepositoryTest {
 
 	@Autowired
 	private TestEntityManager testEntityManager;
+
 	@Autowired
-	private CommentRepository commentRepository;
+	private PostRepository postRepository;
 
 	@Test
-	void saveAndDeleteCommentTest(){
+	void savePostTest(){
 		String content="content";
 		Member writer= new Member("test@gmail.com","encodedPassword","nickname",1,"ewha");
 		testEntityManager.persist(writer);
 		Board board=new Board("오늘 날씨","오늘 날씨 완전 시원하당", writer);
 		testEntityManager.persist(board);
 		boolean anonymous=false;
-		Post post= new Post(content, writer, anonymous, board);
-		testEntityManager.persist(post);
 
-		Comment comment = Comment.builder()
+		Post post = Post.builder()
 			.content(content)
 			.writer(writer)
-			.post(post)
 			.anonymous(anonymous)
+			.board(board)
 			.build();
-		testEntityManager.persist(comment);
+		testEntityManager.persist(post);
+		Optional<Post> postOptional= postRepository.findById(post.getPostId());
 
-		commentRepository.deleteAll();
-		assertThat(commentRepository.findAll()).isEmpty();
+		assertThat(postOptional).isNotEmpty();
 	}
-
-
 }
